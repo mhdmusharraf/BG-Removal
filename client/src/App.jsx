7dd37ee00e7e8bc28a113af1eeb1d29254c6f7cd
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2024-12-28 23:35:32 +0530

    Navbar added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,12 @@ import { Routes,Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Result from './pages/Result'
 import BuyCredit from './pages/BuyCredit'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/result" element={<Result />} />
